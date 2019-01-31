8074261e61581b74fa4189ff582dea93ff0ddf37
Parker Couch <parkercouch@gmail.com>
2019-01-31 14:16:31 -0800

    List and Item interface added to Types

--- src/Types.ts
+++ src/Types.ts
@@ -2,9 +2,23 @@ export interface IUser {
   email: string;
   id: string;
   name: string;
+  lists: string[];
 }
 
 export interface IAuth {
   user: IUser;
   updateUser: (e?: any) => void;
+}
+
+export interface IList {
+  name: string;
+  key: string;
+  creatorId: string;
+  items: IItem[];
+}
+
+export interface IItem {
+  content: string;
+  posterId: string;
+  completed: boolean;
 }

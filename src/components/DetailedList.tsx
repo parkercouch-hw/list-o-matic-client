3c8f5dda827f9f0da7aff826774e51e635f54ec1
Parker Couch <parkercouch@gmail.com>
2019-02-01 10:31:31 -0800

    Added add item to detailed list component

--- src/components/DetailedList.tsx
+++ src/components/DetailedList.tsx
@@ -1,22 +1,69 @@
-import React, { Component } from 'react';
+import axios from 'axios';
+import * as React from 'react';
+import { SERVER_URL } from '../constants/server';
 
 // list is props.location.state
 
-const DetailedList = (props: any) => {
-  const items = props.location.state.items.map((item: any) => (
-    <li key={item._id}>
-      {item.content}
-    </li>
-  ));
+export interface IDetailedListProps {
+  location: any;
+}
 
-  return (
-  <div>
-    <h3>{props.location.state.name}</h3>
-    <ul>
-      {items}
-    </ul>
-  </div>
-  );
-};
+export interface IDetailedListState {
+  newItem: string;
+}
 
-export default DetailedList;
+
+export default class DetailedList extends React.Component<any, any> {
+  public readonly state: IDetailedListState = {
+    newItem: '',
+  };
+
+  public render() {
+    const items = this.props.location.state.items.map((item: any) => (
+      <li key={item._id}>
+        {item.content}
+      </li>
+    ));
+
+    return (
+    <div>
+      <h3>{this.props.location.state.name}</h3>
+      <ul>
+        {items}
+      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <input
+            name="content"
+            placeholder="What else?"
+            value={this.state.newItem}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <input type="submit" value="Add to List" className="button" />
+      </form>
+    </div>
+    );
+  }
+
+  public handleInputChange = (e: any) => { this.setState({ newItem: e.target.value }); };
+
+  public handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const token = localStorage.getItem('serverToken');
+    try {
+      const response = await axios.post(`${SERVER_URL}/lists/addItem`, {
+        content: this.state.newItem,
+        headers: { Authorization: `Bearer ${token}`},
+        listId: this.props.location.state._id,
+        userId: this.props.user.id,
+      });
+      console.log(response);
+    } catch (error) {
+      // TODO: handle errors
+      if (error.response) {
+        console.log(error.response.data.message);
+      }
+    }
+  }
+}
